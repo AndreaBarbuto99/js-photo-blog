817341e9d57c7bfe8c1079c33fafa177dd886b2f
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-01-09 01:15:26 +0100

    feat: added responsive and pin on card

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// const myCard = document.querySelectorAll(".my-card");
-
-// const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
-
-
-// axios.get(endpoint)
-// .then (response => {
-//     const apiDatas = response.data;
-//     myCard.forEach((element, index) => {
-//         if(apiDatas[index]) {
-//         element.innerHTML += `<img class="col-10" src="${apiDatas[index].url}">`
-//         }
-//     });
-
-// })
-
 const myCards = document.querySelectorAll(".my-card");
 
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
@@ -24,7 +8,7 @@ axios.get(endpoint)
     
     myCards.forEach((card, index) => {
         if(apiDatas[index]) {
-            card.innerHTML = `<img class="col-12 mt-3" src="${apiDatas[index].url}">
+            card.innerHTML += `<img class="col-12 mt-3" src="${apiDatas[index].url}">
                               <figcaption class="text-center">${apiDatas[index].title} ${apiDatas[index].date}`;
         }
     });
