d15b772bc78a8c30585025730e7876c8fd98f85d
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-01-12 13:18:06 +0100

    refactor

--- script.js
+++ script.js
@@ -6,30 +6,30 @@ let cardData = [];
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
 
 axios.get(endpoint)
-.then(response => {
-    const apiDatas = response.data;
-    
-    myCards.forEach((card, i) => {
-        if(apiDatas[i]) {
-            cardData[i] = apiDatas[i];
-            card.innerHTML += `<img class="col-12 mt-3" src="${apiDatas[i].url}">
+    .then(response => {
+        const apiDatas = response.data;
+
+        myCards.forEach((card, i) => {
+            if (apiDatas[i]) {
+                cardData[i] = apiDatas[i];
+                card.innerHTML += `<img class="col-12 mt-3" src="${apiDatas[i].url}">
                               <figcaption class="text-center">${apiDatas[i].title} ${apiDatas[i].date}`;
-        }
+            }
+        });
     });
-});
 
-myCards.forEach((card, index) => {
+myCards.forEach((card, i) => {
     card.addEventListener("click", () => {
-        showZoomCard(index); 
+        showZoomCard(i);
     });
 });
 
-function showZoomCard(index) {
-    
+function showZoomCard(i) {
+
     document.getElementById("zoom-container").innerHTML = `
         <div class="zoom-overlay">
             <button class="btn btn-light" onclick="closeZoom()">Chiudi</button>
-            <img src="${cardData[index].url}" alt="${cardData[index].title}">
+            <img src="${cardData[i].url}" alt="${cardData[i].title}">
         </div>
     `;
 }
@@ -41,5 +41,5 @@ function closeZoom() {
 }
 
 function addBlack() {
-myBody.classList.add("body-black");
+    myBody.classList.add("body-black");
 }
